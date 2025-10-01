39d9d6d6db4e2ba35397d7eddb917307f030c548
wani <wani@thegrowthhackers.net>
2025-10-01 17:50:10 +0530

    Set base colors and app icon

---
+++ app/src/main/java/com/wani/sufra/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.wani.sufra.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Red100 = Color(0xFFF85925)
+val Green100 = Color(0xFF0C8F37)
+val Yellow500 = Color(0xFFFC8A0C)
+val Yellow100 = Color(0xFFFFD33E)
+val VanteBlack = Color(0xFF000000)
+val Beige100 = Color(0xFFDBD3CC)
+val White100 = Color(0xFFE6E6E6)
+val Beige500 = Color(0xFF86726C)
